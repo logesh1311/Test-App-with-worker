f4815a20f9fb196dfadac816dfcc5b2f9de341f7
Logesh C <Logesh.c@Triconinfotech.com>
2022-04-14 00:39:09 +0530

    few updates on using worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appWorkerTest';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    console.log(navigator.geolocation)
+    this.http.get('/apis').subscribe(
+      (data: any) => {
+        console.log(data);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+  }
 }
--- workers/index.js
+++ workers/index.js
@@ -1,24 +1,27 @@
 addEventListener("fetch", (event) => {
   event.respondWith(
-    handleRequest(event).catch(
-      (err) => {console.log(err);
-        return new Response(err.stack, { status: 500 })}
+    handleRequest(event.request).catch(
+      (err) => {
+        console.log(err);
+        return new Response(err.stack, { status: 500 })
+      }
     )
   );
 });
 
 
-async function handleRequest(event) {
-  const userCon = event.request.headers.get('cf-ipcountry')
-  const userIp = event.request.headers.get('CF-Connecting-IP')
+async function handleRequest(request) {
   const data = {
-    ip: event.request.headers.get('CF-Connecting-IP'),
-    realip: event.request.headers.get('x-real-ip'),
-    host: event.request.headers.get('host'),
-    country: event.request.headers.get('cf-ipcountry'),
-
+    ip: request.headers.get('CF-Connecting-IP'),
+    realip: request.headers.get('x-real-ip'),
+    host: request.headers.get('host'),
+    country: request.headers.get('cf-ipcountry'),
   }
-  console.log(JSON.stringify(event))
+  console.log(JSON.stringify(request))
   console.log(JSON.stringify(data))
-  return new Response('Your ip is ' + userIp + ' region '+ userCon, {status:200});
+  return new Response(JSON.stringify(data), {
+    status: 200, headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+  });
 }
